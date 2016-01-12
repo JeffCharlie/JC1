c951271d7782dc435d4fbbfb55fac0ab509c6bc1
jeffcharlie <jeff.levinson@gmail.com>
2016-01-12 00:17:07 -0800

    Jeff update 4 Jan 12

--- app/src/main/java/com/jc1/hooli/cafe/CourseDetailActivity.java
+++ app/src/main/java/com/jc1/hooli/cafe/CourseDetailActivity.java
@@ -1,7 +1,7 @@
 package com.jc1.hooli.cafe;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
